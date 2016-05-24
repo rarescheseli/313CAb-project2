3f42311f2ec08f8c9a4162cfa2aa698465852bf4
mhpopescu <mh.popescu12@gmail.com>
2016-05-24 03:03:56 +0300

    Am adaugat metode pentru suma datelor din intervale si numarul de elemente intre 2 noduri ( Se dau ca parametrii cheile nodurilor) In nod se salveaza acum key(timestamp) si data(discount).

--- AVL - rapid/main.cpp
+++ AVL - rapid/main.cpp
@@ -9,10 +9,10 @@ ofstream g("out");
 int main() {
     srand(time(NULL));
 
-    AVLnode<int> *root = new AVLnode<int>(1), *mn, *key;
+    AVLnode<int> *root = new AVLnode<int>(1, 0), *mn, *key;
 
     for (int i = 2; i <= 15; ++i){
-        root = root->insert(i);
+        root = root->insert(i, i);
         mn = root->searchKey(i);
         g << mn->getData() << '\n';
     }
@@ -20,14 +20,20 @@ int main() {
     root -> InOrderDisplay(root);
     cout << '\n';
     root->bfs(root);
+    /*
     cout << root->getSons() << '\n';
     cout << root->getLeftSons() << '\n';
     cout << root->getRightSons() << '\n';
-    cout << root->getSumData() <<'\n';
-    cout << root->LCA(root->searchKey(7), root->searchKey(1))->getData() <<'\n';
-    cout << root->LCA(root->searchKey(12), root->searchKey(1))->getData() <<'\n';
-    cout << root->LCA(root->searchKey(9), root->searchKey(10))->getData() <<'\n';
+    cout << root->getSumData() <<'\n' << '\n';
+    cout << root->LCA(root->searchKey(7), root->searchKey(1))->getKey() <<'\n';
+    cout << root->LCA(root->searchKey(12), root->searchKey(1))->getKey() <<'\n';
+    cout << root->LCA(root->searchKey(9), root->searchKey(10))->getKey() <<'\n';
+    cout << '\n' << root->getIntervalSons(2,15) << '\n';*/
     //cout << root->get
     //cout << root->rightNode->getSons() << '\n';
+
+    cout << '\n' << root->getIntervalSons(8,15) << '\n';
+    cout << '\n' << root->getIntervalData(8,15) << '\n';
+
     delete root;
 }
